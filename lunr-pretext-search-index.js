c620e2f3b5e034624a7440c88af1b8fdff0a718b
GitHub <noreply@github.com>
2025-05-14 22:42:06 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,13 +10,103 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
+  "id": "sec-linear-equations-in-two-variables",
+  "level": "1",
+  "url": "sec-linear-equations-in-two-variables.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Linear Equations In Two Variables",
+  "body": " Linear Equations In Two Variables    Linear Equation in Two Variables is the equation of the form:   where are real numbers. Note that and cannot be both simultaneously. Either can be or can be , but not both at the same.(See what happens if we plug in zeros for both and .)  A quick reminder: the solution to a linear equation in two variables is an ordered pair that makes the equation true.  Every linear equation has infinitely many solutions, forming a straight line on a plane. In other words, the graph of any linear equation in two variables is always a straight line.     Graph of a Linear Equation in Two Variables   graph of a linear function in definition    f(x)=2*x+4       f(x)=2x+4        "
+},
+{
+  "id": "def-linear-equation-in-two-variables",
+  "level": "2",
+  "url": "sec-linear-equations-in-two-variables.html#def-linear-equation-in-two-variables",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  Linear Equation in Two Variables is the equation of the form:   where are real numbers. Note that and cannot be both simultaneously. Either can be or can be , but not both at the same.(See what happens if we plug in zeros for both and .)  A quick reminder: the solution to a linear equation in two variables is an ordered pair that makes the equation true.  Every linear equation has infinitely many solutions, forming a straight line on a plane. In other words, the graph of any linear equation in two variables is always a straight line.   "
+},
+{
+  "id": "graph-of-line",
+  "level": "2",
+  "url": "sec-linear-equations-in-two-variables.html#graph-of-line",
+  "type": "Figure",
+  "number": "1.1.2",
+  "title": "",
+  "body": " Graph of a Linear Equation in Two Variables   graph of a linear function in definition    f(x)=2*x+4       f(x)=2x+4       "
+},
+{
+  "id": "sec-systems-of-linear-equations-into",
+  "level": "1",
+  "url": "sec-systems-of-linear-equations-into.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Introduction to Systems of Linear Equations",
+  "body": " Introduction to Systems of Linear Equations  To begin, recall the definition of Linear Equations In Two Variables by clicking here -     Two-Variable Linear Systems contains two linear equations with two variables.   A solution of a system of equations in two variables is an ordered pair of numbers that is a solution to both equations in the system.     Determine whether is a solution to the system   To answer this question, we must plug the coordinates into the equations to verify that both equations yield true statements. If one of the equations does not, the ordered pair is not a solution.  Since both equations yield true statements, we conclude that is a solution to the system.    Determine whether is a solution to the system   We must plug the coordinates into the equations to verify that both equations yield true statements. If one of the equations does not, the ordered pair is not a solution.  The second equation yielded a false statement, thus is not a solution to the system.   "
+},
+{
+  "id": "def-system-of-linear-equations-in-two-variables",
+  "level": "2",
+  "url": "sec-systems-of-linear-equations-into.html#def-system-of-linear-equations-in-two-variables",
+  "type": "Definition",
+  "number": "2.1.1",
+  "title": "",
+  "body": "  Two-Variable Linear Systems contains two linear equations with two variables.   A solution of a system of equations in two variables is an ordered pair of numbers that is a solution to both equations in the system.   "
+},
+{
+  "id": "sec-systems-of-linear-equations-into-4",
+  "level": "2",
+  "url": "sec-systems-of-linear-equations-into.html#sec-systems-of-linear-equations-into-4",
+  "type": "Example",
+  "number": "2.1.2",
+  "title": "",
+  "body": " Determine whether is a solution to the system   To answer this question, we must plug the coordinates into the equations to verify that both equations yield true statements. If one of the equations does not, the ordered pair is not a solution.  Since both equations yield true statements, we conclude that is a solution to the system.  "
+},
+{
+  "id": "sec-systems-of-linear-equations-into-5",
+  "level": "2",
+  "url": "sec-systems-of-linear-equations-into.html#sec-systems-of-linear-equations-into-5",
+  "type": "Example",
+  "number": "2.1.3",
+  "title": "",
+  "body": " Determine whether is a solution to the system   We must plug the coordinates into the equations to verify that both equations yield true statements. If one of the equations does not, the ordered pair is not a solution.  The second equation yielded a false statement, thus is not a solution to the system.  "
+},
+{
+  "id": "sec-solving-systems-of-linear-equations-by-graphing",
+  "level": "1",
+  "url": "sec-solving-systems-of-linear-equations-by-graphing.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Solving Systems of Linear Equations by Graphing",
+  "body": " Solving Systems of Linear Equations by Graphing  Since a solution of a system of equations is common to both equations, it is also a point common to the graphs of both equations. To find the solution of a system of two linear equations, we graph the equations and see where the lines intersect.   Solve the system of equations by graphing We can graph a linear equation by finding two ordered pair solutions or solving the equation for and using the intercept and the slope.  As the method of graphing is often our choice, I personally like solving for and using the intercept and the slope, especially when the intercept is an integer.  To verify if the intercept is an integer, check if the constant on the right side of the equation is a multiple (divisible) of the coefficient of .  In the first equation, , the constant on the right side, , is divisible by the coefficient of , , indicating that the y-intercept will be an integer. In contrast, in the second equation, , the y-intercept will not be an integer since the constant on the right side, , is not divisible by the coefficient of , .  Thus, to graph the first line, I will solve the equation for , , while for the second, I will calculate two ordered pair solutions (make a table). To solve the first equation for , move the term to the right (flip the sign) and divide out the coefficient of   It is now in the slope-intercept form. To graph it, we start by plotting the intercept, , and then find the additional point(s) using the slope (rise over run, two units up, one to the right).   Graph of the first equation   graph of the first equation unique solution of system of equations by graphing    f(x)=2*x-6       y=2x-6        To graph the second equation, I will find two ordered pairs by plugging a random value for or and solving for the remaining variable. While no rule favors one variable over the other for which we plug the values, checking the coefficients is helpful. If one of the variables in the equation has a coefficient of or , it becomes easier to solve for that variable. Thus, I would plug the values for the other variable and solve for the “easy one.”  In our second equation, , the coefficient of is . Therefore, I'll calculate two ordered pairs by substituting values for and solving for .  Quick reminder: I choose values to at random. Once I calculate the vales, I will record the solutions in the form of a table, and moving forward, I will refer to this approach as the table method.    Graph of the first equation   graph of the first equation unique solution of system of equations by graphing    f(x)=2*x-6  g(x)=-1\/3*x+10\/3         y=2x-6    x+3y=10         "
+},
+{
+  "id": "sec-solving-systems-of-linear-equations-by-graphing-3",
+  "level": "2",
+  "url": "sec-solving-systems-of-linear-equations-by-graphing.html#sec-solving-systems-of-linear-equations-by-graphing-3",
+  "type": "Example",
+  "number": "2.2.1",
+  "title": "",
+  "body": " Solve the system of equations by graphing We can graph a linear equation by finding two ordered pair solutions or solving the equation for and using the intercept and the slope.  As the method of graphing is often our choice, I personally like solving for and using the intercept and the slope, especially when the intercept is an integer.  To verify if the intercept is an integer, check if the constant on the right side of the equation is a multiple (divisible) of the coefficient of .  In the first equation, , the constant on the right side, , is divisible by the coefficient of , , indicating that the y-intercept will be an integer. In contrast, in the second equation, , the y-intercept will not be an integer since the constant on the right side, , is not divisible by the coefficient of , .  Thus, to graph the first line, I will solve the equation for , , while for the second, I will calculate two ordered pair solutions (make a table). To solve the first equation for , move the term to the right (flip the sign) and divide out the coefficient of   It is now in the slope-intercept form. To graph it, we start by plotting the intercept, , and then find the additional point(s) using the slope (rise over run, two units up, one to the right).   Graph of the first equation   graph of the first equation unique solution of system of equations by graphing    f(x)=2*x-6       y=2x-6        To graph the second equation, I will find two ordered pairs by plugging a random value for or and solving for the remaining variable. While no rule favors one variable over the other for which we plug the values, checking the coefficients is helpful. If one of the variables in the equation has a coefficient of or , it becomes easier to solve for that variable. Thus, I would plug the values for the other variable and solve for the “easy one.”  In our second equation, , the coefficient of is . Therefore, I'll calculate two ordered pairs by substituting values for and solving for .  Quick reminder: I choose values to at random. Once I calculate the vales, I will record the solutions in the form of a table, and moving forward, I will refer to this approach as the table method.    Graph of the first equation   graph of the first equation unique solution of system of equations by graphing    f(x)=2*x-6  g(x)=-1\/3*x+10\/3         y=2x-6    x+3y=10        "
+},
+{
   "id": "sec-section-name",
   "level": "1",
   "url": "sec-section-name.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "3.1",
   "title": "Section Title",
   "body": " Section Title  Text of section.  "
+},
+{
+  "id": "ch-logs",
+  "level": "1",
+  "url": "ch-logs.html",
+  "type": "Chapter",
+  "number": "4",
+  "title": "Logarithms are a beautiful soup",
+  "body": " Logarithms are a beautiful soup   Text before the first section.     "
 },
 {
   "id": "backmatter-2",
